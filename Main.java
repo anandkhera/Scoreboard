47dd0ab50c5dab95dcf4041c48cc64cd5f0f14bf
anandkhera <anandk3@nycstudents.net>
2024-10-23 15:49:48 +0000

    added method

--- Main.java
+++ Main.java
@@ -1,7 +1,31 @@
-public class Main
-{
+public class Main {
     public static void main(String[] args) {
-        Scoreboard s = new Scoreboard();
-        System.out.println(s);
+
+        String info;
+        Scoreboard game = new Scoreboard("Red", "Blue");
+        info = game.getScore();
+        System.out.println(info);
+        game.recordPlay(1);
+        info = game.getScore();
+        System.out.println(info);
+        game.recordPlay(0);
+        info = game.getScore();
+        System.out.println(info);
+        game.recordPlay(3);
+        info = game.getScore();
+        System.out.println(info);
+        game.recordPlay(1);
+        game.recordPlay(0);
+        info = game.getScore();
+        System.out.println(info);
+        game.recordPlay(0);
+        game.recordPlay(4);
+        game.recordPlay(0);
+        info = game.getScore();
+        System.out.println(info);
+        
+
+
     }
+    
 }
--- Scoreboard.java
+++ Scoreboard.java
@@ -1,1 +1,32 @@
-public class Scoreboard{}
+public class Scoreboard {
+
+    private String teamOne;
+    private String teamTwo;
+    private int teamOneScore;
+    private int teamTwoScore;
+    private String activeTeam;
+
+    public Scoreboard(String one, String two) {
+        teamOne = one;
+        teamTwo = two;
+        activeTeam = teamOne;
+    }
+
+    public String getScore() {
+        return teamOneScore + "-" + teamTwoScore + "-" + activeTeam;
+    }
+
+    public void recordPlay(int points) {
+        if(teamOne.equals(activeTeam))
+        teamOneScore += points;
+        if(teamTwo.equals(activeTeam))
+        teamTwoScore += points;
+        if(points == 0)
+        {
+            if(activeTeam.equals(teamOne)) activeTeam = teamTwo;
+            else if(teamTwo.equals(activeTeam)) activeTeam = teamOne;
+            
+        }
+    }
+
+}
